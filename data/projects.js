e06de841dd909009660b458af634a37fdba5aa95
Claudiu Ciurdea <claudiu.ciurdea@bytex.ro>
2021-10-23 18:38:15 +0300

    updated project data

---
+++ data/projects.js
@@ -0,0 +1,142 @@
+const companyProjects = [
+	{
+		"id": 1,
+		"name": "Android task monitoring",
+		"logo": "https://digipedia.ro/wp-content/uploads/2017/08/ANDROID.png",
+		"description": "This project is exclusively designed to simplify the tracking and monitoring of day-to-day activities of the busy modern life. We are so busy in our daily lives right now that it becomes impossible to keep track of our daily events, meetings, and appointments. To address this issue, this project focuses on developing a Weekly Task Alerting System for Android devices that can alert users of the tasks/appointments that are scheduled for each day in a week. The project is primarily a reminder app powered by an AI chatbot that reminds users of all the tasks that are due daily. Users can use this application to set goals and achieve them, thereby increasing productivity and simplifying their lives.",
+		"team": [
+			{
+					"name": "Prisha Clay",
+					"role": "Team Lead Developer",
+			},
+			{
+					"name": "Gregg Parrish",
+					"role": "Backend Java Developer",
+			},
+			{
+					"name": "Stacie Torres",
+					"role": "Frontend React Developer",
+			}
+		],
+		"stack": ["Java", "Spring Boot", "Javascript", "React", "MySql"],
+		"openPositions":[
+			{
+				"name": "Frontend React Developer",
+				"level": "Junior",
+			},
+			{
+				"name": "Backend Java Developer",
+				"level": "Junior",
+			}
+		]
+	},
+	{
+		"id": 2,
+		"name": " Sentiment analysis for product rating",
+		"logo": "https://kctbsanalytics.files.wordpress.com/2017/03/capture.png?w=670",
+		"description": "This project aims to develop a sentiment analysis system for product rating. It is an e-commerce web application. The main goal of this sentiment analysis system is to understand the hidden sentiments of customers in feedback and comments and analyze their product rating patterns. When registered customers use this app to view products, product features, and comment on different products, the sentiment analysis system will analyze the comments of various users and ranks products accordingly. The system leverages a database of sentiment-based keywords (including positivity or negativity weight). So, when a user comments on a particular product, the sentiment analysis system analyzes the keywords in the comment to find the match with the keywords stored in the database. After analyzing the matches against the positive and negative keywords and sentiments, the system ranks a product as good, bad, and very bad. Thus, users can use this application to find out reviews on a product.",
+		"team": [
+			{
+					"name": "Bridget Lozano",
+					"role": "Team Lead",
+			},
+			{
+					"name": "Suleman Bryan",
+					"role": "Backend .NET Developer",
+			},
+			{
+					"name": "Stacie Torres",
+					"role": "Frontend Vue Developer",
+			}
+		],
+		"stack": [".NET", ".NET Core", "Javascript", "VueJs", "SQL Server"],
+		"openPositions":[
+			{
+				"name": "Frontend Vue Developer",
+				"level": "Junior",
+			},
+			{
+				"name": "Backend .NET Developer",
+				"level": "Senior",
+			},
+			{
+				"name": "DevOps Azure Engineer",
+				"level": "Mid",
+			}
+		]
+	},
+	{
+		"id": 3,
+		"name": "Fingerprint-based ATM system",
+		"logo": "https://itcbd.com/img/Biometric-ATM.jpg",
+		"description": "This project is a desktop application that uses the fingerprint of users for authentication. Since each individual has a unique fingerprint, this method of using fingerprint as a means of authentication to access your ATM is safer and more secure than using an ATM card. Users need not carry their ATM cards with them at all times – they can use their fingerprint to access ATM services. To use the fingerprint-based ATM system, users have to log in to their account using their fingerprint. After logging in, once they provide their unique pin, they can conduct all kinds of banking transactions, from withdrawing cash to money transfer, and even viewing their account balance. Furthermore, users can also check the last five transactions from their account.",
+		"team": [
+			{
+				"name": "Alfie-Lee Pham",
+				"role": "Team Lead",
+			},
+			{
+				"name": "Vinny Johnson",
+				"role": "Backend Python Developer",
+			},
+			{
+				"name": "Macaulay Craft",
+				"role": "Backend C++ Developer",
+			},
+			{
+				"name": "Khadeeja Andersen",
+				"role": "Devops AWS Engineer",
+			}
+		],
+		"stack": ["Python", "Flask", "C++", "AWS", "Postgres", "Java"],
+		"openPositions":[
+			{
+				"name": "Backend Java Developer",
+				"level": "Sennior",
+			},
+		]
+	},
+	{
+		"id": 4,
+		"name": "Advanced employee management system",
+		"logo": "https://i.ytimg.com/vi/xWDkIK333T4/maxresdefault.jpg",
+		"description": "Usually, large companies and organizations have a vast army of human resources working under them. In such scenarios where there’s a significant number of human employees to manage, it becomes challenging to supervise and manage the activities of the human resources efficiently. Such companies/organizations need an advanced employee management system that can handle everything related to the company’s human resources. This project aims to build an employee management system that consolidates all the relevant information on a company’s human resources. It has two major components – Admin and Employee. The Admin is in charge of the company information, leave management and payroll, and can also add and tweak employee details. Likewise, employees can use the system to check their leave status, view salary details, yearly holiday list, and also for voicing grievances or resigning. This can be one of the final year project ideas for IT students. ",
+	},
+	{
+		"id": 6,
+		"name": "Fingerprint voting system",
+		"logo": "https://www.honestballot.com/wp-content/uploads/2019/11/38646453_S.jpg",
+		"description": "In this project, you will develop a fingerprint-based voting application that can recognize users based on their fingerprint patterns. Since fingerprint authentication is unique for each person, the system can quickly identify the voters using their fingerprints. Naturally, a voter can vote for a candidate only once. The Admin adds all the names and photos of the candidates nominated for the election. The Admin then authenticates users by verifying their identity proof, and after verification, the voter is registered in the system. Furthermore, the Admin even enters the data when the election will end, after which the system will automatically delete the names and photos of the candidates and voters. The users can log in to the system via the unique user ID and password given by the Admin and then use their fingerprints to vote for their preferred candidate. Once the election is over, and the votes are counted, Both the Admin and users can view the election result using the election ID.",
+	},
+	{
+		"id": 9,
+		"name": "Railway tracking and arrival time prediction system",
+		"logo": "https://s.hdnux.com/photos/75/70/61/16223306/5/1200x0.jpg",
+		"description": "This project is a railway tracking system that allows users to obtain relevant information about train timing, whether it is on time or running late, what is the arrival time in a particular station, and so on. The system can track the time of departure of a train from a station and forward this information to the upcoming stations. If the system identifies a delay in the departure of a train from a particular station, it will pass the information on to the next stations. This project delivers real-time train schedule events to multiple subscribing client applications. This tracking system consists of an Admin module. The Admin enters all the relevant details about trains and their arrival timing on different stations, which is then forwarded to the next station via an Internet server. Usually, the Admins add information such as train departed from a station, expected arrival at the next destination, delay in the train schedule, etc. Once the upcoming station receives this data, it will automatically select the data associated with that station and display the same on the screen for the users to see. So, if the Admin at Mumbai station enters information specific to Delhi station, only the Delhi station system will display that information. The station masters at every station have unique login IDs using which they can update the train arrival time at their station. ",
+	},
+	{
+		"id": 11,
+		"name": "Opinion mining for social networking platforms",
+		"logo": "https://makeawebsitehub.com/wp-content/uploads/2016/04/social_media.jpg",
+		"description": "This web application uses opinion mining methodology for improved decision making and enhancing customer experience. The application collects and analyzes the comments and feedback on different users on social networking platforms (Facebook, Instagram, Twitter, etc.) and categorizes them as positive and negative reviews. Similar to the sentiment analysis system, this application, too, has a database containing sentiment-based keywords with positivity or negativity weight. So, it analyzes the keywords mined from the user comments against the keywords stored in the database. Accordingly, the comments are ranked as good, bad, and worst reviews (based on their positive or negative weight). The Admin updates the database with new and relevant keywords. Users can log in to the application to view their status. They can also view the topics and comments posted by other users. By clicking on a particular topic or comment, users can provide their take on the matter. Also read about Top 4 types of sentiment analysis & where to use.",
+	},
+	{
+		"id": 13,
+		"name": "Data leakage detection system",
+		"logo": "https://userscontent2.emaze.com/images/c02fbe92-9ab8-4f05-b539-5c9a60330416/f843041648358f9afc1038f5d99bc8b5.png",
+		"description": "The primary task of data leakage detection systems is to detect data leakage and discover the sources for the same. If ever an organization’s sensitive data gets leaked, it may then finds its way to unauthorized devices. This is why it is crucial to trace the source of data leakage so that you can nip the problem in the bud. An advanced data leakage detection system can prevent data from leaking out of its original source. This project exclusively focuses on designing an improved data leakage detection system. The proposition here is to identify data leakages by storing data as per agents. It can promptly trace any possible sources of unauthorized leakage by leveraging a data allocation strategy across various agents. As a part of the strategy, a user can transfer data to other users via “receivers” – these are agents to whom the data is allocated by using ID-based undetectable alterations. The data leakage detection system uses these alterations to identify the source of leaked data immediately when it is found in any unauthorized places or systems.",
+	},
+	{
+		"id": 14,
+		"name": "Credit card fraud detection",
+		"logo": "https://miro.medium.com/max/601/1*xSqK9iS7nZAaB-Sdwiwjow.png",
+		"description": "Credit card fraud is a significant threat in the BFSI sector. This credit card fraud detection system studies and analyzes user behavior patterns and uses location scanning techniques to identify any unusual patterns. One of The user patterns includes important user behavior like spending habits, usage patterns, etc. The system uses geographic location for identity verification. In case it detects any unusual pattern, the user will be required to undergo the verification process again. The fraud detection system stores the past transaction data of each user. Based on this data, it calculates the standard user behavior patterns for individual users, and any deviation from those normal patterns becomes a trigger for the system. In the instance of any unusual activity, the system will not only raise alerts, but it will also block the user after three invalid attempts.",
+	},
+	{
+		"id": 15,
+		"name": "AI shopping system",
+		"logo": "https://digipedia.ro/wp-content/uploads/2017/08/ANDROID.png",
+		"description": "As the name suggests, the AI multi-agent shopping system is a shopping assistant, more like a recommendation engine. First, the system is fed with the details of a wide range of products. When a user registers in the system and searches for a specific product, the system returns the result of the most relevant items that match the search keywords entered by the user. Since the system is designed to study the search patterns of the user and based on the previous search and purchase history, it will also provide a curated suggestion of a list of items that are a close match to the searched item. The AI shopping system allows users to register and log in using an ID and password. After logging in, users can search for products online, perform a custom search across various product categories, add items to cart, and choose the preferred payment option. ",
+	},
+
+]
---
+++ data/quiz-team-selection.js
@@ -0,0 +1,57 @@
+const quizTeamSelection = {
+    "id": 2,
+    "quizTitle": "Team Selection Quiz",
+    "quizPicture": {
+      "src": "https://image.shutterstock.com/image-photo/selecting-person-job-creating-successful-260nw-1208845708.jpg",
+      "alt": "https://www.slidegeeks.com/pics/dgm/l/t/Team_Selection_Process_Ppt_PowerPoint_Presentation_Clipart_Slide_1-.jpg"
+    },
+    "quizSynopsis": "There are many ways to prevent team selection disputes from arising and many people have an important role to play in this process:",
+    "questions": [
+      {
+        "question": "If someone came to you with a question you saw as trivial, how would you react?",
+        "answers": [
+          "I would try to see the problem from their perspective.",
+          "I would push the problem to someone else.",
+          "They were tolerable; not the best, and not the worst.",
+        ],
+        "correctAnswer": "3"
+      },
+      {
+        "question": "Think about your room at home. How well organized is it?",
+        "answers": [
+          "It's a complete mess.",
+          "Extremely well-organized. Everything has its place.",
+          "It could be better, but it could also be worse.",
+        ],
+        "correctAnswer": "3"
+      },
+      {
+        "question": "How important it is you to see the bigger picture in things?",
+        "answers": [
+          "I always prefer to know exactly what my job is.",
+          "It's not that important, but it does help.",
+          "I'd prefer to just focus on my own thing.",
+        ],
+        "correctAnswer": "3"
+      },
+      {
+        "question": "How do you handle emergencies?",
+        "answers": [
+          "I would prefer to preemptively avoid them.",
+          "I find them extremely stressful.",
+          "I buckle down and get things done.",
+        ],
+        "correctAnswer": "3"
+      },
+      {
+        "question": "How interested are you in continuing your education?",
+        "answers": [
+          "Extremely. I'm a life-long learner.",
+          "I'd like to learn as much as I can, but it isn't a priority.",
+          "I'd prefer to just get out into the field.",
+        ],
+        "correctAnswer": "3"
+      }
+    ]
+  } 
+  
