88aa43ee97be07a3c9e3b61805e7eb65b46d7c4a
SirCQQ <gatucristian@gmail.com>
2021-10-23 16:15:30 +0300

    Added color schema

---
+++ data/colorSchema.js
@@ -0,0 +1,25 @@
+const COLOR_SCHEMAS= {
+  basic:{
+
+  },
+  slytherin:{
+    primary:"#1a472a",
+    secondary:"#000000"
+  },
+  gryfindorr:{
+    primary:"#7f0909",
+    secondary:"#ffc500"
+  },
+  ECB939:{
+    primary:"#ECB939",
+    secondary:"##726255"
+  },
+  ravenclaw:{
+    primary:"#0e1a40",
+    secondary:"#946b2d"
+    
+  },
+
+}
+
+//     https://learnui.design/tools/accessible-color-generator.html
