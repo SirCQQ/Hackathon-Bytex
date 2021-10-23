9ebf708ea2f1298cb7a9c90e4a4fda9fd2c8d6d3
Claudiu Ciurdea <claudiu.ciurdea@bytex.ro>
2021-10-23 16:29:22 +0300

    updated colorSchema

--- data/colorSchema.js
+++ data/colorSchema.js
@@ -4,19 +4,27 @@ const COLOR_SCHEMAS= {
   },
   slytherin:{
     primary:"#1a472a",
-    secondary:"#000000"
+    secondary:"#000000",
+    primaryText:"#ffffff",
+    secondaryText:"#ffffff"
   },
   gryfindorr:{
     primary:"#7f0909",
-    secondary:"#ffc500"
+    secondary:"#ffc500",
+    primaryText:"#ffffff",
+    secondaryText:"#575757",
   },
   ECB939:{
     primary:"#ECB939",
-    secondary:"##726255"
+    secondary:"#726255",
+    primaryText: "#4f4f4f",
+    secondaryText: "#ffffff"
   },
   ravenclaw:{
     primary:"#0e1a40",
-    secondary:"#946b2d"
+    secondary:"#946b2d",
+    primaryText:"#2c2c2c",
+    secondaryText:"#ffffff"
     
   },
 
